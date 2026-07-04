dd48b5d9b3a41e4cfa299dfe4f12314c00872412
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2026-07-03 19:35:36 -0600

    Diseño del Card

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courses.model.Course
 import com.example.courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoursesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    CourseApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +43,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CourseApp(modifier: Modifier = Modifier){}
+
+@Composable
+fun CourseCard(
+    course: Course,
+    modifier: Modifier = Modifier
+){
+    Card(modifier = modifier) {
+        Row() {
+            Image(
+                painter = painterResource(course.imageResourceId),
+                contentDescription = stringResource(course.stringResourceId),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(68.dp)
+            )
+            Column {
+                Text(
+                    text = stringResource(course.stringResourceId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        top = 16.dp,
+                        start = 16.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                    )
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(
+                                start = 16.dp
+                            )
+                    )
+                    Text(
+                        text = course.countTopic.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+            }
+        }
+    }
 }
+
+@Composable
+fun ListOfCourses(
+    courses: List<Course>,
+    modifier: Modifier = Modifier
+){}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CoursesCardPreview() {
     CoursesTheme {
-        Greeting("Android")
+        CourseCard(
+            Course(
+                R.string.photography,
+                321,
+                R.drawable.photography
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/example/courses/model/Course.kt
@@ -0,0 +1,10 @@
+package com.example.courses.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Course(
+    @StringRes val stringResourceId: Int,
+    val countTopic: Int,
+    @DrawableRes val imageResourceId: Int
+)
